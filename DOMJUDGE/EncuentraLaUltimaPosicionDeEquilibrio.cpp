ead2b2df96029829e2aab3cedb77a4f98b669285
Nerea <jguillen.nereajimenez@gmail.com>
2018-10-23 16:33:50 +0200

    Domjudge 23/10/18

---
+++ DOMJUDGE/EncuentraLaUltimaPosicionDeEquilibrio.cpp
@@ -0,0 +1,74 @@
+//NEREA JIMÉNEZ GONZÁLEZ
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int resolver(vector<int> v){
+	int p = -1;
+	int cero = 0, uno = 0;
+	int i = 0;
+
+	//cota v.Length-i;
+	while (i < v.size()){
+		if (v[i] == 0)
+			cero++;			
+		if (v[i] == 1)		
+			uno++;
+		if (cero == uno)
+			p = i;			
+		i++;				 
+	}
+
+
+	return p;
+	//Q:
+	//cero = #k:: 0 <= k < i : v[k]=0
+	//uno = #k:: 0>= k < i : v[k] =1
+	//numCero(v,o, p+1) = numUno(v,0,p+1)
+	//forall z:: p < z < i ==> numCero(v,0,z) != numUno(v,0,z)
+}
+
+
+void resuelveCaso() {
+ //resuelve aqui tu caso
+	int values;
+	int p;
+    //Lee los datos
+	cin >> values;
+	vector<int> v(values);
+	for (int i = 0; i < values; i++){
+		cin >> v[i];
+	}
+    //Calcula el resultado
+	p = resolver(v);
+    //Escribe el resultado
+	cout << p << endl;
+}
+
+
+int main() {
+    // Para la entrada por fichero.
+#ifndef DOMJUDGE
+    std::ifstream in("casos05.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+    
+    
+    unsigned int numCasos;
+    std::cin >> numCasos;
+    // Resolvemos
+    for (int i = 0; i < numCasos; ++i) {
+        resuelveCaso();
+    }
+    
+    
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    
+    return 0;
+}
---
+++ DOMJUDGE/TemperaturasExtremas.cpp
@@ -0,0 +1,70 @@
+//NEREA JIMÉNEZ GONZÁLEZ
+
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void numPicosValles(vector<int> v, int n, int&npicos, int&nvalles){
+	//P: 0 < v.Length <= 10000 ^ forall k: 0 <= k < v.Length-1 ==> -50 <= v[k] <= 60
+	//la precondición NO se debe de comprobar en el código
+	int i = v.size()-1;
+	npicos = 0;
+	nvalles = 0;
+	//cota: i
+	while (i > 1){
+		if ((v[i - 1] > v[i - 2]) && (v[i] < v[i - 1]))
+			npicos++;
+		if ((v[i] > v[i - 1]) && (v[i - 1] < v[i-2]))
+			nvalles++;
+		i--;
+	}
+
+	//Q: 
+	// npicos = #z: 1 <= z < v.Length- : v[z-1] < v[z] > v[z+1]
+	// nvalles = #z: 1 <= z < v.Length-1 : v[z-1] > v[z] < v[z-1]
+	//invariante: 1 <= n <= v.Length-1 ^ npicos = #z: n <= z < v.Length: v[z-1] < v[z] > v[z+1] ^ nvalles = #z: 1 <= z < v.Length-1 : v[z-1] > v[z] < v[z-1]
+}
+
+
+void resuelveCaso() {
+ //resuelve aqui tu caso
+	int npicos, nvalles; //NO DEBEN ESTAR INICIALIZADOS
+	int n;
+    //Lee los datos
+	cin >> n;
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++){
+		cin >> v[i];
+	}
+    //Calcula el resultado
+	numPicosValles(v, n, npicos, nvalles);
+    //Escribe el resultado
+	cout << npicos << " " << nvalles << endl;
+}
+
+int main() {
+    // Para la entrada por fichero.
+#ifndef DOMJUDGE
+    std::ifstream in("casos01.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+    
+    
+    unsigned int numCasos;
+    std::cin >> numCasos;
+    // Resolvemos
+    for (int i = 0; i < numCasos; ++i) {
+        resuelveCaso();
+    }
+    
+    
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    
+    return 0;
+}
